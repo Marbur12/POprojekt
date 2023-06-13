5aba35bf7b6a3b786cec4e523550a791d79ea302
mtttkk <135271972+mtttkk@users.noreply.github.com>
2023-06-13 19:05:54 +0200

    Update Mecz2ogni.java

--- v4(jakbym_cos_zjebal)/Mecz2ogni.java
+++ v4(jakbym_cos_zjebal)/Mecz2ogni.java
@@ -4,4 +4,7 @@ public final class Mecz2ogni extends Mecz implements Serializable {
         Mecz2ogni(Druzyna d0, Druzyna d1, Sedzia s){
         super(d0,d1, s);
     }
+        public String rodzajMeczu(){
+            return "Mecz 2 ogni";
+        }
 }
